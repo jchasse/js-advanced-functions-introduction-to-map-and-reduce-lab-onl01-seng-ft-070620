121ef2d2e1346e5a69df4b396d8e00836129a0c3
jchasse <jonathan.r.chasse@gmail.com>
2020-10-20 13:48:03 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
-// Your code here
+// pry = require('pryjs')
+
+function mapToNegativize(sourceArray) {
+    let newArray = []
+    for (let element of sourceArray) {
+        newArray.push(element * -1)
+    }
+    return newArray
+}
+
+function mapToNoChange(sourceArray) {
+    let newArray = []
+    for (let element of sourceArray) {
+        newArray.push(element)
+    }
+    return newArray
+}
+
+function mapToDouble(sourceArray) {
+    let newArray = []
+    for (let element of sourceArray) {
+        newArray.push(element*2)
+    }
+    return newArray
+}
+
+function mapToSquare(sourceArray) {
+    let newArray = []
+    for (let element of sourceArray) {
+        newArray.push(element**2)
+    }
+    return newArray
+}
+
+function reduceToTotal(sourceArray, startingPoint=0) {
+    let result = startingPoint
+    for (let element of sourceArray) {
+        result += element
+    }
+    return result
+}
+
+function reduceToAllTrue(sourceArray) {
+    let result = true
+    for (let element of sourceArray) {
+        if (!element) return false
+    }
+    return true
+}
+
+function reduceToAnyTrue(sourceArray) {
+    let result = true
+    for (let element of sourceArray) {
+        if (!!element) return true
+    }
+    return false
+}
